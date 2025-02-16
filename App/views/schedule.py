777e614bacb70b16b4017a46d7fb7a0faf0501e3
Drsamzz <sameerinsafali@gmail.com>
2025-02-16 23:06:00 +0000

    Update Schedule Page

--- App/views/schedule.py
+++ App/views/schedule.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from flask_jwt_extended import jwt_required
+from App.controllers.scheduler import help_desk_scheduler
 
 schedule_views = Blueprint('schedule_views', __name__, template_folder='../templates')
 
@@ -7,3 +8,27 @@ schedule_views = Blueprint('schedule_views', __name__, template_folder='../templ
 @jwt_required()
 def schedule():
     return render_template('schedule/view.html')
+
+@schedule_views.route('/api/generate_schedule', methods=['GET'])
+@jwt_required()
+def generate_schedule():
+    try:
+        # Call the scheduler with default parameters
+        # I = 10 staff members
+        # J = 45 shifts
+        # K = 1 course (simplified for now)
+        I, J, K = 10, 45, 1
+        help_desk_scheduler(I, J, K)
+        
+        # For now return dummy data - will need to capture actual scheduler output
+        schedule = [[0 for _ in range(J)] for _ in range(I)]
+        
+        return jsonify({
+            'status': 'success',
+            'schedule': schedule
+        })
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
