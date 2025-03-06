25df869d33cfbe18ea7873d683a7e2ec22aee73b
notjohnnym <jonathan.mohammed.15@gmail.com>
2025-03-06 02:29:19 +0000

    Added allocation and shift classes

---
+++ App/models/availability.py
@@ -0,0 +1,23 @@
+from App.database import db
+
+class Availability(db.model):
+    __tablename__ = 'availability'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), db.ForeignKey('student.username'), nullable=False)
+    shift = db.Column(db.Integer, db.ForeignKey('shift.id'), nullable=False)
+    
+    students = db.relationship('Student', backref=db.backref('availability', lazy=True))
+    shifts = db.relationship('Shift', backref=db.backref('availability', lazy=True))
+    
+    def __init__(self, username, shift):
+        self.username = username
+        self.shift_id = shift
+    
+    def get_json(self):
+        return {
+            'Availability ID': self.id,
+            'Student ID': self.username,
+            'Shift ID': self.shift,
+        }
+    
