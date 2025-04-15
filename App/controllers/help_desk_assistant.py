8826cde93fffd1e1157d65f8327290599075fdb3
notjohnnym <jonathan.mohammed.15@gmail.com>
2025-04-15 05:09:15 +0000

    Fixed function name

--- App/controllers/help_desk_assistant.py
+++ App/controllers/help_desk_assistant.py
@@ -1,11 +1,16 @@
 from App.models import HelpDeskAssistant
 from App.database import db
 
-def create_lab_assistant(username):
+def create_help_desk_assistant(username):
     new_assistant = HelpDeskAssistant(username=username)
     db.session.add(new_assistant)
     db.session.commit()
     return new_assistant
 
-def get_lab_assistant(username):
+
+def get_help_desk_assistant(username):
     return HelpDeskAssistant.query.filter_by(username=username).first()
+
+
+def get_active_help_desk_assistants():
+    return HelpDeskAssistant.query.filter_by(active=True).all()
